ca22646a175b4835e94c8513692f4fa88b8e37f0
Piera Marchesini <pieracarolina@yahoo.com.br>
2018-03-07 12:03:04 -0300

    Serialization with JSONDecoder is completed

---
+++ SeriesMaze/SeriesMaze/Model/Serie.swift
@@ -0,0 +1,27 @@
+//
+//  Serie.swift
+//  SeriesMaze
+//
+//  Created by Piera Marchesini on 06/03/18.
+//  Copyright © 2018 Piera Marchesini. All rights reserved.
+//
+
+import Foundation
+
+struct Serie: Decodable {
+    let score: Float
+    let show: Show
+}
+
+struct Show: Decodable {
+    let name: String
+    let genres: [String]
+    let premiered: String
+    let image: Image?
+    let summary: String
+}
+
+struct Image: Decodable {
+    let medium: String
+    let original: String
+}
